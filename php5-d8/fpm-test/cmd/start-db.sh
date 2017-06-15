b4b872b44ba7bdbc2dae68936ea7fd81fee2ca88
坚硬的石头 <do_nb@outlook.com>
2017-06-15 09:36:13 +0800

    [php5-d8] 修改 fpm 数据库初始化

--- php5-d8/fpm-test/cmd/start-db.sh
+++ php5-d8/fpm-test/cmd/start-db.sh
@@ -4,6 +4,7 @@ if [ ! -d "$DATADIR/mysql" ]; then
     chown -R mysql:mysql /var/lib/mysql
     service mysql start
     mysqladmin -uroot password "mysql"
+    echo "use mysql;update user set host='%' where user='root';flush privileges;" |mysql -uroot -pmysql
 fi
 service mysql start
 tail -f /var/log/mysql/error.log
